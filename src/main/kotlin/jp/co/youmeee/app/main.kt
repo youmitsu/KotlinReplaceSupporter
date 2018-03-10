9b7c1d41c6c5e51da63e0ef93bc74f8048a08956
三堀　裕 <yumitsuhori@sanhorihiroshi-no-MacBook-Pro.local>
2018-03-11 00:20:03 +0900

    gradleの修正と引数ない場合のエラーハンドリング

--- src/main/kotlin/jp/co/youmeee/app/main.kt
+++ src/main/kotlin/jp/co/youmeee/app/main.kt
@@ -1,9 +1,11 @@
 package jp.co.youmeee.app
 
 fun main(args: Array<String>) {
-    println(System.getProperty("user.dir"))
+    if(args.isEmpty()) {
+        println("Error: Please input an argument which is a targeted directory.")
+        return
+    }
     val rootDirArg: String = args[0]
-    if(rootDirArg.isEmpty()) return
 
     val javaList = SourceList(Language("Java", "java"))
     val kotlinList = SourceList(Language("Kotlin", "kt"))
@@ -11,8 +13,7 @@ fun main(args: Array<String>) {
     val handler = SearchHandler(rootDirArg, arrayOf(javaList, kotlinList))
     handler.execute()
     val sizeAll = (javaList.list.size + kotlinList.list.size).toDouble()
-//    println(javaList.list.size)
-//    println(kotlinList.list.size)
+
     println("------Java------")
     println("ファイル数: ${javaList.list.size}")
     println("------Kotlin------")
