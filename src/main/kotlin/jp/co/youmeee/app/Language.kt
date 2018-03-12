58c638e39a7fa6e8bb5aaef3afe01bda3ef647e5
三堀　裕 <yumitsuhori@sanhorihiroshi-no-MacBook-Pro.local>
2018-03-13 00:09:58 +0900

    LanguageのtoStringをoverride

--- src/main/kotlin/jp/co/youmeee/app/Language.kt
+++ src/main/kotlin/jp/co/youmeee/app/Language.kt
@@ -7,4 +7,6 @@ package jp.co.youmeee.app
 data class Language(
         val name: String,  //言語名
         val extension: String //拡張子
-        )
+        ){
+        override fun toString() = name
+}
